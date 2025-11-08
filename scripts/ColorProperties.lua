35e4703ea3b54acfffecb3ea7cbe83143272bdf6
Total <39064108+TotalTakeover@users.noreply.github.com>
2025-11-08 01:14:57 -0700

    Shiny/Color overhaul
    - Script cleanup

--- scripts/ColorProperties.lua
+++ scripts/ColorProperties.lua
@@ -1,121 +1,20 @@
--- Required scripts
-local pokemonParts = require("lib.GroupIndex")(models.SerperiorTaur)
-local itemCheck    = require("lib.ItemCheck")
+-- Avatar color
+avatar:color(vectors.hexToRGB("21A64C"))
 
--- Config setup
-config:name("SerperiorTaur")
-local shiny = config:load("ShinyToggle") == nil and vec(client.uuidToIntArray(avatar:getUUID())).x % 4096 == 0 or config:load("ShinyToggle")
+-- Glowing outline
+renderer:outlineColor(vectors.hexToRGB("21A64C"))
 
--- All shiny parts
-local shinyParts = {
-	
-	-- Ears
-	pokemonParts.Ears,
-	pokemonParts.EarsSkull,
-	
-	-- Accessories
-	pokemonParts.NeckLeaves,
-	pokemonParts.LeafArms,
-	pokemonParts.TailLeaf1,
-	pokemonParts.TailLeaf2,
-	pokemonParts.TailLeaf3,
-	
-	-- Tail
-	pokemonParts.Neck.Neck,
-	pokemonParts.Tail1.Tail,
-	pokemonParts.Tail2.Tail,
-	pokemonParts.Tail3.Tail,
-	pokemonParts.Tail4.Tail,
-	pokemonParts.Tail5.Tail,
-	pokemonParts.Tail6.Tail,
-	pokemonParts.Tail7.Tail,
-	pokemonParts.Tail8.Tail,
-	pokemonParts.Tail9.Tail
-	
-}
+-- Host only instructions
+if not host:isHost() then return end
 
 -- Table setup
-local t = {}
+local c = {}
 
-function events.TICK()
-	
-	-- Set colors
-	t.hover     = vectors.hexToRGB(shiny and "4C8CA7" or "21A64C")
-	t.active    = vectors.hexToRGB(shiny and "DDE791" or "EFC435")
-	t.primary   = "#"..(shiny and "4C8CA7" or "21A64C")
-	t.secondary = "#"..(shiny and "DDE791" or "EFC435")
-	
-	-- Shiny textures
-	local textureType = shiny and (textures["textures.serperior_shiny"] or textures["models.SerperiorTaur.serperior_shiny"]) or (textures["textures.serperior"] or textures["models.SerperiorTaur.serperior"])
-	for _, part in ipairs(shinyParts) do
-		--part:primaryTexture("Custom", textureType)
-	end
-	
-	-- Glowing outline
-	renderer:outlineColor(vectors.hexToRGB(shiny and "4C8CA7" or "21A64C"))
-	
-	-- Avatar color
-	avatar:color(vectors.hexToRGB(shiny and "4C8CA7" or "21A64C"))
-	
-end
+-- Action variables
+c.hover     = vectors.hexToRGB("21A64C")
+c.active    = vectors.hexToRGB("EFC435")
+c.primary   = "#21A64C"
+c.secondary = "#EFC435"
 
--- Shiny toggle
-local function setShiny(boolean)
-	
-	shiny = boolean
-	config:save("ColorShiny", shiny)
-	if player:isLoaded() and shiny then
-		sounds:playSound("block.amethyst_block.chime", player:getPos())
-	end
-	
-end
-
--- Sync variables
-local function syncColor(a)
-	
-	shiny = a
-	
-end
-
--- Pings setup
-pings.setColorShiny = setShiny
-pings.syncColor     = syncColor
-
--- Sync on tick
-if host:isHost() then
-	function events.TICK()
-		
-		if world.getTime() % 200 == 0 then
-			pings.syncColor(shiny)
-		end
-		
-	end
-end
-
--- Activate actions
-setShiny(shiny)
-
-t.shinyPage = action_wheel:newAction()
-	:item(itemCheck("gunpowder"))
-	:toggleItem(itemCheck("glowstone_dust"))
-	:onToggle(pings.setColorShiny)
-	:toggled(shiny)
-
--- Update action page info
-function events.TICK()
-	
-	t.shinyPage
-		:title(toJson(
-			{
-				"",
-				{text = "Toggle Shiny Textures\n\n", bold = true, color = t.primary},
-				{text = "Set the lower body to use shiny textures over the default textures.", color = t.secondary}
-			}
-		))
-		:hoverColor(t.hover)
-		:toggleColor(t.active)
-	
-end
-
--- Return table
-return t
+-- Return variables
+return c
---
+++ scripts/Shiny.lua
@@ -0,0 +1,152 @@
+-- Required script
+local parts = require("lib.PartsAPI")
+
+-- Config setup
+config:name("SerperiorTaur")
+local shiny = config:load("ShinyToggle") == nil and vec(client.uuidToIntArray(avatar:getUUID())).x % 4096 == 0 or config:load("ShinyToggle")
+
+-- All shiny parts
+local shinyParts = parts:createTable(function(part) return part:getName():find("_[sS]hiny") end)
+
+-- Variables
+local wasShiny = not shiny
+local initAvatarColor = vectors.hexToRGB(avatar:getColor() or "default")
+local initGlowColor = renderer:getOutlineColor() or vec(1, 1, 1)
+
+-- Textures
+local normalTex = textures["textures.serperior"]       or textures["SerperiorTaur.serperior"]
+local shinyTex  = textures["textures.serperior_shiny"] or textures["SerperiorTaur.serperior_shiny"]
+
+function events.RENDER(delta, context)
+	
+	-- Shiny textures
+	if shiny ~= wasShiny then
+		for _, part in ipairs(shinyParts) do
+			part:primaryTexture("CUSTOM", shiny and shinyTex or normalTex)
+		end
+	end
+	
+	-- Store data
+	wasShiny = shiny
+	
+	-- Avatar color
+	avatar:color(shiny and vectors.hexToRGB("4C8CA7") or initAvatarColor)
+	
+	-- Glowing outline
+	renderer:outlineColor(shiny and vectors.hexToRGB("4C8CA7") or initGlowColor)
+	
+end
+
+-- Shiny toggle
+function pings.setShinyToggle(boolean)
+	
+	shiny = boolean
+	config:save("ShinyToggle", shiny)
+	if player:isLoaded() and shiny then
+		sounds:playSound("block.amethyst_block.chime", player:getPos())
+	end
+	
+end
+
+-- Sync variable
+function pings.syncShiny(a)
+	
+	shiny = a
+	
+end
+
+-- Host only instructions
+if not host:isHost() then return end
+
+-- Sync on tick
+function events.TICK()
+	
+	if world.getTime() % 200 == 0 then
+		pings.syncShiny(shiny)
+	end
+	
+end
+
+-- Required scripts
+local s, wheel, itemCheck, c = pcall(require, "scripts.ActionWheel")
+if not s then return end -- Kills script early if ActionWheel.lua isnt found
+pcall(require, "scripts.Pokeball") -- Tries to find script, not required
+
+-- Dont preform if color properties is empty
+if next(c) ~= nil then
+	
+	-- Store init colors
+	local initColors = {}
+	for k, v in pairs(c) do
+		initColors[k] = v
+	end
+	
+	-- Create shiny colors
+	local shinyColors = {
+		hover     = vectors.hexToRGB("4C8CA7"),
+		active    = vectors.hexToRGB("DDE791"),
+		primary   = "#4C8CA7",
+		secondary = "#DDE791"
+	}
+	
+	-- Update action wheel colors
+	function events.RENDER(delta, context)
+		
+		for k in pairs(c) do
+			c[k] = shiny and shinyColors[k] or initColors[k]
+		end
+		
+	end
+	
+end
+
+-- Check for if page already exists
+local pageExists = action_wheel:getPage("Serperior")
+
+-- Pages
+local parentPage    = action_wheel:getPage("Main")
+local serperiorPage = pageExists or action_wheel:newPage("Serperior")
+
+-- Actions table setup
+local a = {}
+
+-- Actions
+if not pageExists then
+	a.pageAct = parentPage:newAction()
+		:item(itemCheck("cobblemon:leaf_stone", "dandelion"))
+		:onLeftClick(function() wheel:descend(serperiorPage) end)
+end
+
+a.shinyAct = serperiorPage:newAction()
+	:item(itemCheck("gunpowder"))
+	:toggleItem(itemCheck("glowstone_dust"))
+	:onToggle(pings.setShinyToggle)
+
+-- Update actions
+function events.RENDER(delta, context)
+	
+	if action_wheel:isEnabled() then
+		if a.pageAct then
+			a.pageAct
+				:title(toJson(
+					{text = "Serperior Settings", bold = true, color = c.primary}
+				))
+		end
+		
+		a.shinyAct
+			:title(toJson(
+				{
+					"",
+					{text = "Toggle Shiny Textures\n\n", bold = true, color = c.primary},
+					{text = "Toggles the usage of shiny textures for your pokemon parts.", color = c.secondary}
+				}
+			))
+			:toggled(shiny)
+		
+		for _, act in pairs(a) do
+			act:hoverColor(c.hover):toggleColor(c.active)
+		end
+		
+	end
+	
+end
